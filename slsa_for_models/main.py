c44908a0792a7344f40066e950afafbb9896786f
Mihai Maruseac <mihai.maruseac@gmail.com>
2023-10-21 09:43:12 -1000

    Add SLSA for ML example (#36)
    * Train TF CIFAR10 model
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Train Pytorch CIFAR10 model
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Add python requirements file
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Add workflow to generate SLSA provenance
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Reduce verbosity of training TF model to speed up GHA
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Be consistent in quoting strings
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Only save one single format
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Use a single trainer
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Use lazy loading to prevent interference
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Optimize TF model
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Only train TF model for 16 epochs as gains after this point are minimal
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Change workflow to take model format as argument
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    * Generate SLSA provenance
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>
    
    ---------
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

---
+++ slsa_for_models/main.py
@@ -0,0 +1,47 @@
+# Copyright Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing perepo_managerissions and
+# limitations under the License.
+
+import argparse
+
+import tensorflow_cifar10 as tf
+import pytorch_cifar10 as pt
+
+
+def readOptions():
+  parser = argparse.ArgumentParser('Train CIFAR10 models with TF/PT')
+  model_formats = list(tf.supported_models().keys())
+  model_formats += list(pt.supported_models().keys())
+  parser.add_argument('model', choices=model_formats,
+                      help='Model to generate (name implies framework)')
+  return parser.parse_args()
+
+
+def main(args):
+  model_formats = list(tf.supported_models().keys())
+  for model_format in model_formats:
+    if args.model == model_format:
+      return tf.model_pipeline(args.model)
+
+  model_formats = list(pt.supported_models().keys())
+  for model_format in model_formats:
+    if args.model == model_format:
+      return pt.model_pipeline(args.model)
+
+  # we should not reach this case in the normal flow, but cover all corners
+  raise ValueError("Model format not supported")
+
+
+if __name__ == '__main__':
+  args = readOptions()
+  main(args)
