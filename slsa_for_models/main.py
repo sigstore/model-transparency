8b3d9cc4a82a3c7f61ec7479f6ea90647b223bff
Mihai Maruseac <mihaimaruseac@google.com>
2023-10-24 05:08:58 -0700

    Fix linting on SLSA for models
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- slsa_for_models/main.py
+++ slsa_for_models/main.py
@@ -19,29 +19,29 @@ import pytorch_cifar10 as pt
 
 
 def readOptions():
-  parser = argparse.ArgumentParser('Train CIFAR10 models with TF/PT')
-  model_formats = list(tf.supported_models().keys())
-  model_formats += list(pt.supported_models().keys())
-  parser.add_argument('model', choices=model_formats,
-                      help='Model to generate (name implies framework)')
-  return parser.parse_args()
+    parser = argparse.ArgumentParser('Train CIFAR10 models with TF/PT')
+    model_formats = list(tf.supported_models().keys())
+    model_formats += list(pt.supported_models().keys())
+    parser.add_argument('model', choices=model_formats,
+                        help='Model to generate (name implies framework)')
+    return parser.parse_args()
 
 
 def main(args):
-  model_formats = list(tf.supported_models().keys())
-  for model_format in model_formats:
-    if args.model == model_format:
-      return tf.model_pipeline(args.model)
+    model_formats = list(tf.supported_models().keys())
+    for model_format in model_formats:
+        if args.model == model_format:
+            return tf.model_pipeline(args.model)
 
-  model_formats = list(pt.supported_models().keys())
-  for model_format in model_formats:
-    if args.model == model_format:
-      return pt.model_pipeline(args.model)
+    model_formats = list(pt.supported_models().keys())
+    for model_format in model_formats:
+        if args.model == model_format:
+            return pt.model_pipeline(args.model)
 
-  # we should not reach this case in the normal flow, but cover all corners
-  raise ValueError("Model format not supported")
+    # we should not reach this case in the normal flow, but cover all corners
+    raise ValueError("Model format not supported")
 
 
 if __name__ == '__main__':
-  args = readOptions()
-  main(args)
+    args = readOptions()
+    main(args)
