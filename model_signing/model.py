b90a14ed5daf971388ba2b6115f072a785f3339f
laurentsimon <laurentsimon@google.com>
2023-10-25 20:27:19 +0000

    print identity and providr during signing
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- model_signing/model.py
+++ model_signing/model.py
@@ -17,6 +17,7 @@ from sigstore.sign import (
 )
 from sigstore._internal.oidc import (
     DEFAULT_AUDIENCE,
+    Identity
 )
 from sigstore.oidc import (
     Issuer,
@@ -37,7 +38,7 @@ from pathlib import Path
 from typing import Optional
 from serialize import Serializer
 import psutil
-
+import sys
 
 def chunk_size() -> int:
     return int(psutil.virtual_memory().available // 2)
@@ -58,7 +59,6 @@ class BaseResult:
 
 class SignatureResult(BaseResult):
     pass
-
 
 class SigstoreSigner():
     def __init__(self,
@@ -102,6 +102,11 @@ class SigstoreSigner():
             token = self.get_identity_token()
             if not token:
                 raise ValueError("No identity token supplied or detected!")
+            oidc_identity = Identity(token)
+
+            # Print identity used to sign the model.
+            print(f"identity-provider: {oidc_identity.issuer}", file=sys. stderr)
+            print(f"identity: {oidc_identity.proof}", file=sys. stderr)
 
             contentio = io.BytesIO(Serializer.serialize_v1(
                 inputfn, chunk_size(), signaturefn, ignorepaths))
