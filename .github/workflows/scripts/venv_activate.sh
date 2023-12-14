918ab7a4b63237c9d431545cedb5751171d74689
Mihai Maruseac <mihaimaruseac@google.com>
2023-12-14 09:16:52 -0800

    Make CI run and test Windows deps on model signing
    For future PRs (as they require more playing with GHA to fix):
    - support Windows deps on SLSA
    - support Windows deps on unit tests (for signing)
    - minimize dependencies
    - add unit tests for SLSA
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- .github/workflows/scripts/venv_activate.sh
+++ .github/workflows/scripts/venv_activate.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 # shellcheck source=/dev/null
-source venv/bin/activate || source venv/Scripts/activate
+if [[ -f venv/bin/activate ]]; then
+  source venv/bin/activate
+elif [[ -f venv/Scripts/activate ]]; then
+  source venv/Scripts/activate
+else
+  echo "Cannot activate venv sandbox. Failing"
+  exit 1
+fi
+
+echo "Successfully activated venv sandbox. Python is at `which python`"
--- .github/workflows/scripts/venv_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m venv venv
-
-# shellcheck source=/dev/null
-source venv/bin/activate || source venv/Scripts/activate
