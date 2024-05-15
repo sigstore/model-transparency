4a530785aa55fd36f149d2b178e8eaf2f0a8da19
Mihai Maruseac <mihaimaruseac@google.com>
2024-05-15 12:32:28 -0700

    Update copyright stanzas to match Sigstore. (#178)
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- model_signing/model.py
+++ model_signing/model.py
@@ -1,4 +1,4 @@
-# Copyright Google LLC
+# Copyright 2024 The Sigstore Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
