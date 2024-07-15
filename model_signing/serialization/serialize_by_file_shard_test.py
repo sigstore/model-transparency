73b40d631b0f905390e442e76cd39e1f33a8ab4a
Mihai Maruseac <mihaimaruseac@google.com>
2024-07-15 15:28:06 -0700

    Remove unused imports
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- model_signing/serialization/serialize_by_file_shard_test.py
+++ model_signing/serialization/serialize_by_file_shard_test.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import pathlib
-import pytest
 
 from model_signing.hashing import file
 from model_signing.hashing import memory
