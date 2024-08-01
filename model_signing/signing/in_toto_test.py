fce6087a74c56e4bd4b69555f899be839830e680
Mihai Maruseac <mihaimaruseac@google.com>
2024-08-01 10:27:01 -0700

    Add in-toto format for items with hash of hashes. (#264)
    This converts model serialization manifests that record every model file
    hash into an in-toto payload that can then be passed to Sigstore's
    `sign_intoto` for signing to generate a Sigstore `Bundle` (if using Sigstore).
    
    To identify the models, we compute a hash of all hashes of the files and
    use that as the subject. The individual file hashes are used as the
    payload and we would have the verifier check them as part of the
    verification process.
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- model_signing/signing/in_toto_test.py
+++ model_signing/signing/in_toto_test.py
@@ -20,6 +20,8 @@ files. If the golden tests are failing, regenerate the golden files with
   pytest model_signing/ --update_goldens
 """
 
+import pathlib
+
 from google.protobuf import json_format
 from in_toto_attestation.v1 import statement_pb2
 import pytest
@@ -85,3 +87,59 @@ class TestSingleDigestIntotoPayload:
 
         with pytest.raises(TypeError, match="Only DigestManifest is supported"):
             in_toto.SingleDigestIntotoPayload.from_manifest(manifest)
+
+
+class TestDigestOfDigestsIntotoPayload:
+
+    def _hasher_factory(self, path: pathlib.Path) -> file.FileHasher:
+        return file.SimpleFileHasher(path, memory.SHA256())
+
+    @pytest.mark.parametrize("model_fixture_name", test_support.all_test_models)
+    def test_known_models(self, request, model_fixture_name):
+        # Set up variables (arrange)
+        testdata_path = request.path.parent / "testdata"
+        test_path = testdata_path / "in_toto"
+        test_class_path = test_path / "TestDigestOfDigestsIntotoPayload"
+        golden_path = test_class_path / model_fixture_name
+        should_update = request.config.getoption("update_goldens")
+        model = request.getfixturevalue(model_fixture_name)
+
+        # Compute payload (act)
+        serializer = serialize_by_file.ManifestSerializer(
+            self._hasher_factory, allow_symlinks=True
+        )
+        manifest = serializer.serialize(model)
+        payload = in_toto.DigestOfDigestsIntotoPayload.from_manifest(manifest)
+
+        # Compare with golden, or write to golden (approximately "assert")
+        if should_update:
+            with open(golden_path, "w", encoding="utf-8") as f:
+                f.write(f"{json_format.MessageToJson(payload.statement.pb)}\n")
+        else:
+            with open(golden_path, "r", encoding="utf-8") as f:
+                json_contents = f.read()
+                expected_proto = json_format.Parse(
+                    json_contents, statement_pb2.Statement()
+                )
+
+            assert payload.statement.pb == expected_proto
+
+    def test_produces_valid_statements(self, sample_model_folder):
+        serializer = serialize_by_file.ManifestSerializer(
+            self._hasher_factory, allow_symlinks=True
+        )
+        manifest = serializer.serialize(sample_model_folder)
+
+        payload = in_toto.DigestOfDigestsIntotoPayload.from_manifest(manifest)
+
+        payload.statement.validate()
+
+    def test_only_runs_on_expected_manifest_types(self):
+        digest = hashing.Digest("test", b"test_digest")
+        manifest = manifest_module.DigestManifest(digest)
+
+        with pytest.raises(
+            TypeError,
+            match="Only FileLevelManifest is supported",
+        ):
+            in_toto.DigestOfDigestsIntotoPayload.from_manifest(manifest)
