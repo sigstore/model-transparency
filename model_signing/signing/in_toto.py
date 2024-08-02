b4a4f6ad7d0687eee046e66258c209ec59509f8c
Mihai Maruseac <mihaimaruseac@google.com>
2024-08-02 09:33:48 -0700

    Add in-toto format with hashes of shards as subjects (#267)
    This converts model serialization manifests that record every model file
    shard hash into an in-toto payload that can then be passed to Sigstore's
    `sign_intoto` for signing to generate a Sigstore `Bundle` (if using
    Sigstore).
    
    This time, we record every hash as part of the subject instead of in the
    payload. We require verifiers to be aware of this and acknowledge that
    verifiers that only check subject by subject (that is, they check if the
    hash of a passed in argument is in the list of subjects and don't check
    if all the hashes are present), can fail to fully detect if the model
    integrity is compromised by renaming one file in the model,
    interchanging two file names, deleting a file, or reordering two shards.
    The signing library will have additional checks for this, but verifying
    the signature with other tools might result in invalid results.
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- model_signing/signing/in_toto.py
+++ model_signing/signing/in_toto.py
@@ -481,3 +481,108 @@ class DigestsIntotoPayload(IntotoPayload):
             manifest, predicate_type=cls.predicate_type
         )
         return cls(statement)
+
+
+class ShardDigestsIntotoPayload(IntotoPayload):
+    """In-toto payload where the subjects are the model shards themselves.
+
+    This payload is supposed to be used for manifests where every file shard in
+    the model is matched with a digest. Because existing tooling only supports
+    established hashing algorithms, we annotate every subject with the actual
+    hash algorithm used to compute the file digest, and use "sha256" as the
+    algorithm name in the digest itself.
+
+    Example:
+    ```json
+    {
+      "_type": "https://in-toto.io/Statement/v1",
+      "subject": [
+        {
+          "name": "d0/d1/d2/d3/d4/f0:0:16",
+          "digest": {
+            "sha256": "6efa14..."
+          },
+          "annotations": {
+            "actual_hash_algorithm": "file-sha256-1000000"
+          }
+        },
+        {
+          "name": "d0/d1/d2/d3/d4/f1:0:16",
+          "digest": {
+            "sha256": "a9bc14..."
+          },
+          "annotations": {
+            "actual_hash_algorithm": "file-sha256-1000000"
+          }
+        },
+        {
+          "name": "d0/d1/d2/d3/d4/f2:0:16",
+          "digest": {
+            "sha256": "5f597e..."
+          },
+          "annotations": {
+            "actual_hash_algorithm": "file-sha256-1000000"
+          }
+        },
+        {
+          "name": "d0/d1/d2/d3/d4/f3:0:16",
+          "digest": {
+            "sha256": "eaf677..."
+          },
+          "annotations": {
+            "actual_hash_algorithm": "file-sha256-1000000"
+          }
+        }
+      ],
+      "predicateType": "https://model_signing/ShardDigests/v0.1",
+      "predicate": {
+        "unused": "Unused, just passed due to API requirements"
+      }
+    }
+    ```
+
+    If the annotation for a subject is missing, or it does not contain
+    actual_hash_algorithm, it should be assumed that the digest is computed via
+    the algorithm listed in the digest dictionary (i.e., sha256).
+
+    See also https://github.com/sigstore/sigstore-python/issues/1018.
+    """
+
+    predicate_type: Final[str] = (
+        "https://model_signing/ShardDigests/v0.1"
+    )
+
+    def __init__(self, statement: statement.Statement):
+        """Builds an instance of this in-toto payload.
+
+        Don't call this directly in production. Use `from_manifest()` instead.
+
+        Args:
+            statement: The DSSE statement representing this in-toto payload.
+        """
+        self.statement = statement
+
+    @classmethod
+    @override
+    def from_manifest(cls, manifest: manifest_module.Manifest) -> Self:
+        """Converts a manifest to the signing payload used for signing.
+
+        The manifest must be one where every model shard is paired with its own
+        digest. Currently, this is only `ShardLevelManifest`.
+
+        Args:
+            manifest: the manifest to convert to signing payload.
+
+        Returns:
+            An instance of `DigestOfDigestsIntotoPayload`.
+
+        Raises:
+            TypeError: If the manifest is not `ShardLevelManifest`.
+        """
+        if not isinstance(manifest, manifest_module.ShardLevelManifest):
+            raise TypeError("Only ShardLevelManifest is supported")
+
+        statement = _convert_descriptors_to_direct_statement(
+            manifest, predicate_type=cls.predicate_type
+        )
+        return cls(statement)
