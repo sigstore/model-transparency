6cb9ff1912f1930ce6d1ac8dd6eba3fa323b1aba
Martin Sablotny <msablotny@nvidia.com>
2025-01-23 16:07:07 -0800

    small bugfix do not indicate wrong results (#348)
    * small bugfix do not indicate wrong results
    
    Signed-off-by: Martin Sablotny <msablotny@nvidia.com>
    
    * comment
    
    Signed-off-by: Martin Sablotny <msablotny@nvidia.com>
    
    ---------
    
    Signed-off-by: Martin Sablotny <msablotny@nvidia.com>

--- src/verify.py
+++ src/verify.py
@@ -17,6 +17,7 @@
 import argparse
 import logging
 import pathlib
+import sys
 
 from model_signing import model
 from model_signing.hashing import file
@@ -168,6 +169,7 @@ def main():
         )
     except verifying.VerificationError as err:
         log.error(f"verification failed: {err}")
+        sys.exit(-1)
 
     log.info("all checks passed")
 
