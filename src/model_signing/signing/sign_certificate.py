cd3df25d35dddad4db730c65acf54e6e44ccd506
Mihai Maruseac <mihaimaruseac@google.com>
2025-04-03 15:07:40 -0700

    Add traditional signing signers and verifyiers.
    First, we add a new `sign_sigstore_pb.py` interface which is mostly the
    same as `sign_sigstore.py` except that it uses the sigstore protobuf
    specs instead of using `sigstore-python`. Since `sigstore-python` only
    assumes signing with Sigstore, we have to do this.
    
    Then, we add abilities to sign with key pairs and with certificates.
    
    This resolves several bugs that existed in the `signature/`
    implementations, making sure to properly check everything and to build a
    PAE that is correctly build (proper capitalization, use bytes, by type
    correct).
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

---
+++ src/model_signing/signing/sign_certificate.py
@@ -0,0 +1,187 @@
+# Copyright 2025 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Signers and verifiers using certificates."""
+
+from collections.abc import Iterable
+import json
+import pathlib
+
+import certifi
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.x509 import oid
+from OpenSSL import crypto
+from sigstore_protobuf_specs.dev.sigstore.bundle import v1 as bundle_pb
+from sigstore_protobuf_specs.dev.sigstore.common import v1 as common_pb
+from typing_extensions import override
+
+from model_signing.signing import sign_ec_key as ec_key
+from model_signing.signing import sign_sigstore_pb as sigstore_pb
+
+
+class Signer(ec_key.Signer):
+    """Signer using certificates."""
+
+    def __init__(
+        self,
+        private_key_path: pathlib.Path,
+        signing_certificate_path: pathlib.Path,
+        certificate_chain_paths: Iterable[pathlib.Path],
+    ):
+        """Initializes the signer with the key, certificate and trust chain.
+
+        Args:
+            private_key_path: The path to the PEM encoded private key.
+            signing_certificate_path: The path to the signing certificate.
+            certificate_chain_paths: Paths to other certificates used to
+              establish chain of trust.
+
+        Raises:
+            ValueError: Signing certificate's public key does not match the
+              private key's public pair.
+        """
+        super().__init__(private_key_path)
+        self._signing_certificate = x509.load_pem_x509_certificate(
+            signing_certificate_path.read_bytes()
+        )
+
+        public_key_from_key = self._private_key.public_key()
+        public_key_from_certificate = self._signing_certificate.public_key()
+        if public_key_from_key != public_key_from_certificate:
+            raise ValueError(
+                "The public key from the certificate does not match "
+                "the public key paired with the private key"
+            )
+
+        self._trust_chain = x509.load_pem_x509_certificates(
+            b"".join([path.read_bytes() for path in certificate_chain_paths])
+        )
+
+    @override
+    def _get_verification_material(self) -> bundle_pb.VerificationMaterial:
+        def _to_protobuf_certificate(certificate):
+            return common_pb.X509Certificate(
+                raw_byutes=certificate.public_bytes(
+                    encoding=serialization.Encoding.DER
+                )
+            )
+
+        chain = [_to_protobuf_certificate(self._signing_certificate)]
+        chain.extend(
+            [
+                _to_protobuf_certificate(certificate)
+                for certificate in self._trust_chain
+            ]
+        )
+
+        return bundle_pb.VerificationMaterial(
+            x509_certificate_chain=common_pb.X509CertificateChain(
+                certificates=chain
+            )
+        )
+
+
+class Verifier(sigstore_pb.Verifier):
+    """Verifier for signatures generated via signing with certificates."""
+
+    def __init__(
+        self, certificate_chain_paths: Iterable[pathlib.Path] = frozenset()
+    ):
+        """Initializes the verifier with the list of certificates to use.
+
+        Args:
+            certificate_chain_paths: Paths to certificates used to verify
+              signature and establish chain of trust. By default this is empty,
+              in which case we would use the root certificates from the
+              operating system, as per `certifi.where()`.
+        """
+        if not certificate_chain_paths:
+            certificate_chain_paths = [certifi.where()]
+
+        certificates = x509.load_pem_x509_certificates(
+            b"".join([path.read_bytes() for path in certificate_chain_paths])
+        )
+
+        self._store = crypto.X509Store()
+        for certificate in certificates:
+            self._store.add_cert(crypto.X509.from_cryptography(certificate))
+
+    @override
+    def _verify_bundle(self, bundle: bundle_pb.Bundle) -> tuple[str, bytes]:
+        public_key = self._verify_certificates(bundle.verification_material)
+        envelope = bundle.dsse_envelope
+        public_key.verify(
+            envelope.signatures[0].sig,
+            sigstore_pb.pae(envelope.payload),
+            ec.ECDSA(ec.key.get_ec_key_hash(public_key)),
+        )
+
+        return envelope.payload_type, json.loads(envelope.payload)
+
+    def _verify_certificates(
+        self, verification_material: bundle_pb.VerificationMaterial
+    ) -> ec.EllipticCurvePublicKey:
+        """Verifies the certificate chain and returns the public key.
+
+        The public key is extracted from the signing certificate from the chain
+        of trust, after the chain is validated. It must match the public key
+        from the key used during signing.
+        """
+
+        def _to_openssl_certificate(certificate_bytes):
+            return crypto.X509.from_cryptography(
+                x509.load_der_x509_certificate(certificate_bytes)
+            )
+
+        signing_chain = verification_material.x509_certificate_chain
+        signing_certificate = x509.load_der_x509_certificate(
+            signing_chain.certificates[0].raw_bytes
+        )
+
+        max_signing_time = signing_certificate.not_valid_before
+        self._store.set_time(max_signing_time)
+
+        signing_certificate_ssl = _to_openssl_certificate(
+            signing_chain.certificates[0].raw_bytes
+        )
+        trust_chain_ssl = [
+            _to_openssl_certificate(certificate.raw_bytes)
+            for certificate in signing_chain.certificates[1:]
+        ]
+        store_context = crypto.X509StoreContext(
+            self._store, signing_certificate_ssl, trust_chain_ssl
+        )
+        store_context.verify_certificate()
+
+        extensions = signing_certificate.extensions
+        can_use_for_signing = False
+        usage = extensions.get_extension_for_class(x509.KeyUsage)
+        if usage.value.digital_signature:
+            can_use_for_signing = True
+        else:
+            try:
+                usage = extensions.get_extension_for_class(
+                    x509.ExtendedKeyUsage
+                )
+                if oid.ExtendedKeyUsageOID.CODE_SIGNING in usage.value:
+                    can_use_for_signing = True
+            except x509.ExtensionNotFound:
+                pass
+
+        if not can_use_for_signing:
+            raise ValueError("Signing certificate cannot be used for signing")
+
+        return signing_certificate.public_key()
---
+++ src/model_signing/signing/sign_ec_key.py
@@ -0,0 +1,179 @@
+# Copyright 2025 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Signers and verifiers using elliptic curve keys."""
+
+import json
+import pathlib
+from typing import Optional
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from google.protobuf import json_format
+from sigstore_protobuf_specs.dev.sigstore.bundle import v1 as bundle_pb
+from sigstore_protobuf_specs.dev.sigstore.common import v1 as common_pb
+from sigstore_protobuf_specs.io import intoto as intoto_pb
+from typing_extensions import override
+
+from model_signing.signing import sign_sigstore_pb as sigstore_pb
+from model_signing.signing import signing
+
+
+def _check_supported_ec_key(public_key: ec.EllipticCurvePublicKey):
+    """Checks if the elliptic curve key is supported by our package.
+
+    We only support a family of curves, trying to match those specified by
+    Sigstore's protobuf specs.
+    See https://github.com/sigstore/model-transparency/issues/385.
+
+    Args:
+        public_key: The public key to check. Can be obtained from a private key.
+
+    Raises:
+        ValueError: The key is not supported.
+    """
+    curve = public_key.curve.name
+    if curve not in ["secp256r1", "secp384r1", "secp521r1"]:
+        raise ValueError(f"Unsupported key for curve '{curve}'")
+
+
+def get_ec_key_hash(
+    public_key: ec.EllipticCurvePublicKey,
+) -> hashes.HashAlgorithm:
+    """Returns the public key hashing algorithm.
+
+    We need to record this in the sigstore bundle when signing and retrieve when
+    performing verification. This is according to sigstore protobuf specs and is
+    used both when signing with keys and when signing with certificates.
+
+    Args:
+        public_key: The public key to get the hash algorithm from.
+
+    Raises:
+        ValueError: The key is not supported.
+    """
+    key_size = public_key.curve.key_size
+
+    # TODO: Once Python 3.9 support is deprecated revert to using `match`
+    if key_size == 256:
+        return hashes.SHA256()
+    if key_size == 384:
+        return hashes.SHA384()
+    if key_size == 521:
+        return hashes.SHA512()
+
+    raise ValueError(f"Unexpected key size {key_size}")
+
+
+class Signer(sigstore_pb.Signer):
+    """Signer using an elliptic curve private key."""
+
+    def __init__(
+        self, private_key_path: pathlib.Path, password: Optional[str] = None
+    ):
+        """Initializes the signer with the private key and optional password.
+
+        Args:
+            private_key_path: The path to the PEM encoded private key.
+            password: Optional password for the private key.
+        """
+        self._private_key = serialization.load_pem_private_key(
+            private_key_path.read_bytes(), password
+        )
+        _check_supported_ec_key(self._private_key.public_key())
+
+    @override
+    def sign(self, payload: signing.Payload) -> signing.Signature:
+        raw_payload = json_format.MessageToJson(payload.statement.pb).encode(
+            "utf-8"
+        )
+
+        raw_signature = intoto_pb.Signature(
+            sig=self._private_key.sign(
+                sigstore_pb.pae(raw_payload),
+                ec.ECDSA(get_ec_key_hash(self._private_key.public_key())),
+            ),
+            keyid=None,
+        )
+
+        envelope = intoto_pb.Envelope(
+            payload=raw_payload,
+            payload_type=signing._IN_TOTO_JSON_PAYLOAD_TYPE,
+            signatures=[raw_signature],
+        )
+
+        return sigstore_pb.Signature(
+            bundle_pb.Bundle(
+                media_type=sigstore_pb._BUNDLE_MEDIA_TYPE,
+                verification_material=self._get_verification_material(),
+                dsse_envelope=envelope,
+            )
+        )
+
+    def _get_verification_material(self) -> bundle_pb.VerificationMaterial:
+        """Returns the verification material to include in the bundle."""
+        public_key = self._private_key.public_key()
+        key_size = public_key.curve.key_size
+
+        # TODO: Once Python 3.9 support is deprecated revert to using `match`
+        if key_size == 256:
+            key_details = common_pb.PublicKeyDetails.PKIX_ECDSA_P256_SHA_256
+        elif key_size == 384:
+            key_details = common_pb.PublicKeyDetails.PKIX_ECDSA_P384_SHA_384
+        elif key_size == 521:
+            key_details = common_pb.PublicKeyDetails.PKIX_ECDSA_P521_SHA_512
+        else:
+            raise ValueError(f"Unexpected key size {key_size}")
+
+        return bundle_pb.VerificationMaterial(
+            public_key=common_pb.PublicKey(
+                raw_bytes=public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                ),
+                key_details=key_details,
+            )
+        )
+
+
+class Verifier(sigstore_pb.Verifier):
+    """Verifier for signatures generated with an elliptic curve private key."""
+
+    def __init__(self, public_key_path: pathlib.Path):
+        """Initializes the verifier with the public key to use.
+
+        Args:
+            public_key_path: The path to the public key to use. This must be
+              paired with the private key used to generate the signature.
+        """
+        self._public_key = serialization.load_pem_public_key(
+            public_key_path.read_bytes()
+        )
+        _check_supported_ec_key(self._public_key)
+
+    @override
+    def _verify_bundle(self, bundle: bundle_pb.Bundle) -> tuple[str, bytes]:
+        # TODO: This method currently does not verify that the public key
+        # matches what is included in the signature's verification material.
+        # Instead, the verification material is completely ignored right now.
+
+        envelope = bundle.dsse_envelope
+        self._public_key.verify(
+            envelope.signatures[0].sig,
+            sigstore_pb.pae(envelope.payload),
+            ec.ECDSA(get_ec_key_hash(self._public_key)),
+        )
+
+        return envelope.payload_type, json.loads(envelope.payload)
