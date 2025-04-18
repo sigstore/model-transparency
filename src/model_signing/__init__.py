d56521f123fa0ad14127f40ed831334cd47a80e8
Mihai Maruseac <mihaimaruseac@google.com>
2025-04-18 10:53:39 -0700

    Prepare for v1.0.1 patch release (#447)
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- src/model_signing/__init__.py
+++ src/model_signing/__init__.py
@@ -125,7 +125,7 @@ from model_signing import signing
 from model_signing import verifying
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 __all__ = ["hashing", "signing", "verifying", "manifest"]
