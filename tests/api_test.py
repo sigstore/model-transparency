2b8608e455d098ff6039bcf1a1837bd1313f9228
Stefan Berger <stefanb@linux.ibm.com>
2025-05-21 11:25:00 -0400

    Move and extend python tests (#463)
    * hashing: Use Path.resolve() rather than os.path.relpath()
    
    On Windows os.path.relpath() may create problems if the given path is on
    a different drive than the current working directory. Therefore, use
    the Path's resolve() function to normalize weird paths.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    * tests: Move python sign+verify test into api_test.py
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    * tests: Have git related files ignored
    
    Extend the test cases with checks whether git related files are properly
    ignored. Create a .gitignore file in the model directory and ensure that
    is part of the signature when ignore_git_paths = False, and not part of
    the signature when ignore_git_paths = True.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    * tests: Add checks for the recorded list of ignored paths
    
    Extend the test cases and check that the recorded list of ignore_paths
    in the signature is as expected. Add a non-existing file to the list
    and have it ignored.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    * tests: Check that the proper model name is in the subject name
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    * tests: Check that the proper model name is in the subject name (bash)
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    ---------
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- tests/api_test.py
+++ tests/api_test.py
@@ -15,18 +15,81 @@
 """Tests for the top level API."""
 
 from base64 import b64decode
+from collections.abc import Iterable
 from datetime import datetime
 from datetime import timedelta
 import json
 import os
+from pathlib import Path
 import subprocess
 from tempfile import TemporaryDirectory
 import time
 
 import pytest
 
+from model_signing import hashing
 from model_signing import signing
 from model_signing import verifying
+
+
+# Directory with testdata for this test
+TESTDATA = Path(__file__).parent / "../scripts/tests"
+
+# The default set of git related files that are ignored
+GIT_IGNORE_PATHS: Iterable[str] = [
+    ".git",
+    ".gitattributes",
+    ".gitignore",
+    ".github",
+]
+
+
+@pytest.fixture
+def base_path() -> Path:
+    return Path(__file__).parent
+
+
+@pytest.fixture
+def populate_tmpdir(tmp_path: Path) -> Path:
+    Path(tmp_path / "signme-1").write_text("signme-1")
+    Path(tmp_path / "signme-2").write_text("signme-2")
+    Path(tmp_path / ".gitignore").write_text(".foo")
+    return tmp_path
+
+
+def get_signed_files(modelsig: Path) -> list[str]:
+    with open(modelsig, "r") as file:
+        signature = json.load(file)
+    payload = json.loads(b64decode(signature["dsseEnvelope"]["payload"]))
+    return [entry["name"] for entry in payload["predicate"]["resources"]]
+
+
+def get_ignore_paths(modelsig: Path) -> list[str]:
+    with open(modelsig, "r") as file:
+        signature = json.load(file)
+    payload = json.loads(b64decode(signature["dsseEnvelope"]["payload"]))
+    ignore_paths = payload["predicate"]["serialization"]["ignore_paths"]
+    ignore_paths.sort()
+    return ignore_paths
+
+
+def check_ignore_paths(
+    modelsig: Path,
+    ignore_git_paths: bool,
+    ignore_paths: Iterable[str] = frozenset(),
+) -> None:
+    ignore_paths = list(ignore_paths)
+    if ignore_git_paths:
+        ignore_paths += GIT_IGNORE_PATHS
+    ignore_paths.sort()
+    assert ignore_paths == get_ignore_paths(modelsig)
+
+
+def get_model_name(modelsig: Path) -> str:
+    with open(modelsig, "r") as file:
+        signature = json.load(file)
+    payload = json.loads(b64decode(signature["dsseEnvelope"]["payload"]))
+    return payload["subject"][0]["name"]
 
 
 _MIN_VALIDITY = timedelta(minutes=1)
@@ -101,3 +164,145 @@ class TestSigstoreSigning:
             oidc_issuer=expected_oidc_issuer,
             use_staging=True,
         ).verify(sample_model_folder, signature_path)
+
+        assert [
+            "d0/f00",
+            "d0/f01",
+            "d0/f02",
+            "d1/f10",
+            "d1/f11",
+            "d1/f12",
+            "f0",
+            "f1",
+            "f2",
+            "f3",
+        ] == get_signed_files(signature_path)
+        check_ignore_paths(signature_path, True, [])
+        assert get_model_name(signature_path) == os.path.basename(
+            sample_model_folder
+        )
+
+
+class TestKeySigning:
+    def test_sign_and_verify(self, base_path, populate_tmpdir):
+        os.chdir(base_path)
+
+        model_path = populate_tmpdir
+        ignore_paths = []
+        ignore_git_paths = False
+        signature = Path(model_path / "model.sig")
+        private_key = Path(TESTDATA / "keys/certificate/signing-key.pem")
+        password = None
+
+        signing.Config().use_elliptic_key_signer(
+            private_key=private_key, password=password
+        ).set_hashing_config(
+            hashing.Config().set_ignored_paths(
+                paths=list(ignore_paths) + [signature],
+                ignore_git_paths=ignore_git_paths,
+            )
+        ).sign(model_path, signature)
+
+        public_key = Path(TESTDATA / "keys/certificate/signing-key-pub.pem")
+
+        verifying.Config().use_elliptic_key_verifier(
+            public_key=public_key
+        ).set_hashing_config(
+            hashing.Config().set_ignored_paths(
+                paths=list(ignore_paths) + [signature],
+                ignore_git_paths=ignore_git_paths,
+            )
+        ).verify(model_path, signature)
+
+        assert [".gitignore", "signme-1", "signme-2"] == get_signed_files(
+            signature
+        )
+        check_ignore_paths(signature, ignore_git_paths, ["model.sig"])
+        assert get_model_name(signature) == os.path.basename(model_path)
+
+        # Ignore git paths and other files now
+        ignore_paths = [Path(model_path / "ignored")]
+        ignore_git_paths = True
+
+        signing.Config().use_elliptic_key_signer(
+            private_key=private_key, password=password
+        ).set_hashing_config(
+            hashing.Config().set_ignored_paths(
+                paths=list(ignore_paths) + [signature],
+                ignore_git_paths=ignore_git_paths,
+            )
+        ).sign(model_path, signature)
+
+        assert ["signme-1", "signme-2"] == get_signed_files(signature)
+        check_ignore_paths(
+            signature, ignore_git_paths, ["model.sig", "ignored"]
+        )
+        assert get_model_name(signature) == os.path.basename(model_path)
+
+
+class TestCertificateSigning:
+    def test_sign_and_verify(self, base_path, populate_tmpdir):
+        os.chdir(base_path)
+
+        model_path = populate_tmpdir
+        ignore_paths = []
+        ignore_git_paths = False
+        signature = Path(model_path / "model.sig")
+        private_key = Path(TESTDATA / "keys/certificate/signing-key.pem")
+        signing_certificate = Path(
+            TESTDATA / "keys/certificate/signing-key-cert.pem"
+        )
+        certificate_chain = [
+            Path(TESTDATA / "keys/certificate/int-ca-cert.pem")
+        ]
+        log_fingerprints = False
+
+        signing.Config().use_certificate_signer(
+            private_key=private_key,
+            signing_certificate=signing_certificate,
+            certificate_chain=certificate_chain,
+        ).set_hashing_config(
+            hashing.Config().set_ignored_paths(
+                paths=list(ignore_paths) + [signature],
+                ignore_git_paths=ignore_git_paths,
+            )
+        ).sign(model_path, signature)
+
+        certificate_chain = [Path(TESTDATA / "keys/certificate/ca-cert.pem")]
+
+        verifying.Config().use_certificate_verifier(
+            certificate_chain=certificate_chain,
+            log_fingerprints=log_fingerprints,
+        ).set_hashing_config(
+            hashing.Config().set_ignored_paths(
+                paths=list(ignore_paths) + [signature],
+                ignore_git_paths=ignore_git_paths,
+            )
+        ).verify(model_path, signature)
+
+        assert [".gitignore", "signme-1", "signme-2"] == get_signed_files(
+            signature
+        )
+        check_ignore_paths(signature, ignore_git_paths, ["model.sig"])
+        assert get_model_name(signature) == os.path.basename(model_path)
+
+        # Ignore git paths now
+        ignore_paths = [Path(model_path / "ignored")]
+        ignore_git_paths = True
+
+        signing.Config().use_certificate_signer(
+            private_key=private_key,
+            signing_certificate=signing_certificate,
+            certificate_chain=certificate_chain,
+        ).set_hashing_config(
+            hashing.Config().set_ignored_paths(
+                paths=list(ignore_paths) + [signature],
+                ignore_git_paths=ignore_git_paths,
+            )
+        ).sign(model_path, signature)
+
+        assert ["signme-1", "signme-2"] == get_signed_files(signature)
+        check_ignore_paths(
+            signature, ignore_git_paths, ["model.sig", "ignored"]
+        )
+        assert get_model_name(signature) == os.path.basename(model_path)
