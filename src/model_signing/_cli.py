254dafb8054c716d20ebe0e152f1177ebce089a6
Aleks <121458075+SequeI@users.noreply.github.com>
2025-05-22 18:09:50 +0100

    feat(oauth): add --oauth_force_oob flag to support manual OAuth flow (#471)
    Signed-off-by: SequeI <asiek@redhat.com>

--- src/model_signing/_cli.py
+++ src/model_signing/_cli.py
@@ -211,6 +211,15 @@ def _sign() -> None:
         "from OIDC flow or from the environment."
     ),
 )
+@click.option(
+    "--oauth_force_oob",
+    is_flag=True,
+    default=False,
+    help=(
+        "Force an out-of-band OAuth flow and do not automatically start "
+        "the default web browser."
+    ),
+)
 def _sign_sigstore(
     model_path: pathlib.Path,
     ignore_paths: Iterable[pathlib.Path],
@@ -218,6 +227,7 @@ def _sign_sigstore(
     signature: pathlib.Path,
     use_ambient_credentials: bool,
     use_staging: bool,
+    oauth_force_oob: bool,
     identity_token: Optional[str] = None,
 ) -> None:
     """Sign using Sigstore (DEFAULT signing method).
@@ -241,6 +251,7 @@ def _sign_sigstore(
             use_ambient_credentials=use_ambient_credentials,
             use_staging=use_staging,
             identity_token=identity_token,
+            force_oob=oauth_force_oob,
         ).set_hashing_config(
             model_signing.hashing.Config().set_ignored_paths(
                 paths=list(ignore_paths) + [signature],
