b0e839c65a5784843a51d67438898d095d7f1d6d
Stefan Berger <stefanb@linux.ibm.com>
2025-10-09 20:52:02 -0500

    tests: Add test cases for signatures created by v1.1.0
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ scripts/tests/test-verify-v1.1.0-certificate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Testing 'verify certificate'"
+if ! python -m model_signing \
+	verify certificate \
+	--ignore-paths ./v1.1.0-certificate/ignore-me \
+	--signature ./v1.1.0-certificate/model.sig \
+	--certificate_chain ./keys/certificate/ca-cert.pem \
+	./v1.1.0-certificate/; then
+	echo "Error: 'verify certificate' failed on v1.1.0"
+	exit 1
+fi
+
+exit 0
