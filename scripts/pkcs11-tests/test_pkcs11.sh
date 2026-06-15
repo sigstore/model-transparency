2be55e6d49f4c036bb7ca78d9aeadccafb546e5b
Stefan Berger <stefanb@linux.ibm.com>
2026-06-15 11:57:07 -0400

    tests: Add a pkcs11-certificate test case
    If the openssl command line tool is available then create a CA and sign
    the HSM's public key with it and then test signing and verifying with
    pkcs11-certificate method.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- scripts/pkcs11-tests/test_pkcs11.sh
+++ scripts/pkcs11-tests/test_pkcs11.sh
@@ -21,8 +21,9 @@ model_sig=${TMPDIR}/model.sig
 pub_key=${TMPDIR}/pubkey.pem
 model_path=${TMPDIR}
 
-if ! msg=$(softhsm_setup getpubkey > "${pub_key}"); then
+if ! softhsm_setup getpubkey &>"${pub_key}"; then
 	echo -e "Could not get public key:\n${msg}"
+	echo "${pub_key}"
 	exit 77
 fi
 
@@ -40,6 +41,65 @@ if ! python -m model_signing verify key \
 	"${model_path}"; then
 	echo "Could not verify signature."
 	exit 77
+fi
+
+if type -P openssl >/dev/null; then
+	pub_key_cert=${TMPDIR}/pubkey-cert.pem
+	ca_key="${TMPDIR}/ca-key.pem"
+	ca_cert="${TMPDIR}/ca-cert.pem"
+	v3ext="${TMPDIR}/v3.ext"
+
+	if ! err=$(openssl req \
+		-new \
+		-x509 \
+		-nodes \
+		-days 3650\
+		-subj "/CN=MyRootCA" \
+		-keyout "${ca_key}" \
+		-addext "basicConstraints=critical,CA:TRUE" \
+		-addext "keyUsage=critical,keyCertSign" \
+		-out "${ca_cert}" 2>&1); then
+		echo "Could not create CA certificate."
+		echo "${err}"
+		exit 77
+	fi
+	echo "Created CA."
+
+	cat <<- _EOF_ > "${v3ext}"
+	keyUsage=critical, digitalSignature
+	_EOF_
+
+	if ! err=$(openssl x509 \
+		-new \
+		-CAkey "${ca_key}" \
+		-CA "${ca_cert}" \
+		-force_pubkey "${pub_key}" \
+		-subj "/CN=MyCery" \
+		-extfile "${v3ext}" \
+		-out "${pub_key_cert}" 2>&1); then
+		echo "Could not create certificate for HSM public key."
+		echo "${err}"
+		exit 77
+	fi
+	echo "Signed HSM public key with CA key."
+
+	if ! python -m model_signing sign pkcs11-certificate \
+		--signature "${model_sig}" \
+		--pkcs11_uri "${pkcs11uri}" \
+		--signing_certificate "${pub_key_cert}" \
+		--certificate_chain "${pub_key_cert}" \
+		"${model_path}"; then
+		echo "Could not sign with pkcs11-certificate method."
+		exit 77
+	fi
+
+	if ! python -m model_signing verify certificate \
+		--signature "${model_sig}" \
+		--certificate_chain "${ca_cert}" \
+		"${model_path}"; then
+		echo "Could not verify signature created with pkcs11-certificate method."
+		exit 77
+	fi
 fi
 
 exit 0
